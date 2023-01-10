b56a742c102704a2e58eeb20848f2a0ce4ac0feb
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-10 18:12:17 -0300

    chore: Install cors e helmet

--- app/server/src/application/app.ts
+++ app/server/src/application/app.ts
@@ -1,6 +1,10 @@
 import express from "express"
+import cors from "cors"
+import helmet from "helmet"
 
 const app = express()
+app.use(cors())
+app.use(helmet())
 app.use(express.json())
 
 app.use("/login", () => console.log("Henrique"))
