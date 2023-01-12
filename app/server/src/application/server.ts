460a0a3b40b41e861a258d3ed184c60f231a8758
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-12 15:31:38 -0300

    feat(Server): Criando conexaão com DB

--- app/server/src/application/server.ts
+++ app/server/src/application/server.ts
@@ -1,5 +1,14 @@
 import "dotenv/config"
 import app from "./app"
+import connectToDatabase from "../infrastructure/database/config/connection"
 
-const PORT = process.env.PORT
-app.listen(PORT, () => console.log(`Running server on port: ${PORT}`))
+connectToDatabase()
+  .then(() => {
+    app.listen(process.env.PORT, () =>
+      console.log(`Running server on port: ${process.env.PORT}`)
+    )
+  })
+  .catch((error) => {
+    console.error(error)
+    process.exit(0)
+  })
