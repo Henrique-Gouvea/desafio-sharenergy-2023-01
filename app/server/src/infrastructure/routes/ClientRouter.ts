d0249f87cbe613f47f0fe5e4f00112c3c7a53dad
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-13 14:21:07 -0300

    feat(Router): Adicioanndo controller na route client

--- app/server/src/infrastructure/routes/ClientRouter.ts
+++ app/server/src/infrastructure/routes/ClientRouter.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express"
-import clientController from "../controllers/clientController"
+import clientController from "../integration/ClientIntegration"
 
 const routeClient = Router()
 
