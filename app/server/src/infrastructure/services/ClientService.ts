a9d857f7d24b981cfc05ef5b4ef834160eedd800
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-13 08:44:15 -0300

    feat(Validation): Criando validação para cliente

--- app/server/src/infrastructure/services/ClientService.ts
+++ app/server/src/infrastructure/services/ClientService.ts
@@ -1,10 +1,13 @@
+import CustomError from "../helpers/errors/CustomErrors"
 import { IClient } from "../../interfaces/IClient"
 import { IModel } from "../../interfaces/IModel"
+import clientValidate from "../validation/clientSchema"
 
 class ClientService implements IModel<IClient> {
   constructor(private _modelClient: IModel<IClient>) {}
 
   public async create(client: IClient) {
+    clientValidate(client)
     const newclient = await this._modelClient.create(client)
     return newclient
   }
@@ -16,17 +19,21 @@ class ClientService implements IModel<IClient> {
 
   public async readOne(id: string) {
     const client = await this._modelClient.readOne(id)
+    if (!client) throw new CustomError(404, "Client not found")
     return client
   }
 
   public async update(id: string, client: IClient) {
+    clientValidate(client)
     const getClient = await this._modelClient.readOne(id)
+    if (!getClient) throw new CustomError(404, "Client not found")
     const updateClient = await this._modelClient.update(id, client)
     return updateClient
   }
 
   public async delete(id: string) {
     const getclient = await this._modelClient.readOne(id)
+    if (!getclient) throw new CustomError(404, "Client not found")
     const deletedclient = await this._modelClient.delete(id)
     return deletedclient
   }
---
+++ app/server/src/infrastructure/validation/clientSchema.ts
@@ -0,0 +1,38 @@
+import * as Joi from "joi"
+import { IClient } from "../../interfaces/IClient"
+import CustomError from "../helpers/errors/CustomErrors"
+
+const clientSchema = Joi.object({
+  username: Joi.string().required().messages({
+    "string.empty": "Field is name required",
+    "any.required": "Field is name required",
+  }),
+
+  email: Joi.string().email().required().messages({
+    "string.empty": "Field is email required",
+    "any.required": "Field is email required",
+  }),
+
+  phone: Joi.string().required().messages({
+    "string.empty": "Field is phone required",
+    "any.required": "Field is phone required",
+  }),
+
+  adress: Joi.string().required().messages({
+    "string.empty": "Field is adress required",
+    "any.required": "Field is adress required",
+  }),
+
+  cpf: Joi.string().min(6).required().messages({
+    "string.empty": "Field is cpf required",
+    "any.required": "Field is cpf required",
+  }),
+})
+
+const clientValidate = (client: IClient): void => {
+  const { error } = clientSchema.validate(client)
+
+  if (error) throw new CustomError(400, error.message)
+}
+
+export default clientValidate
