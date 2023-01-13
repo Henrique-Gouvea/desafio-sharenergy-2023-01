f4adf09539fb83ed3963432fae15fdd7c7627b28
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-13 19:55:38 -0300

    feat(Validation): Criando validação para usuario

---
+++ app/server/src/infrastructure/validation/userValidation.ts
@@ -0,0 +1,23 @@
+import * as Joi from "joi"
+import { IUser } from "../../interfaces/IUser"
+import CustomError from "../helpers/errors/CustomErrors"
+
+const userSchema = Joi.object({
+  username: Joi.string().required().messages({
+    "string.empty": "Field is name required",
+    "any.required": "Field is name required",
+  }),
+
+  password: Joi.string().email().required().messages({
+    "string.empty": "Field is password required",
+    "any.required": "Field is password required",
+  }),
+})
+
+const userValidate = (user: IUser): void => {
+  const { error } = userSchema.validate(user)
+
+  if (error) throw new CustomError(400, error.message)
+}
+
+export default userValidate
