a7186800ad604ed80fca2fa171adef749d685e72
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-13 21:08:50 -0300

    feat(Provider): Adicionando token

---
+++ app/server/src/infrastructure/providers/tokenProvider.ts
@@ -0,0 +1,34 @@
+import { JwtPayload, sign, verify } from "jsonwebtoken"
+import "dotenv/config"
+import { IToken } from "../interfaces/providers/IToken"
+import CustomError from "../helpers/errors/CustomErrors"
+
+class tokenProvider implements IToken {
+  private jwtSecret: string
+  constructor() {
+    this.jwtSecret = process.env.JWT_SECRET || "jwt_secret"
+  }
+
+  generateToken(email: string): string {
+    const token = sign({ data: email }, this.jwtSecret, {
+      expiresIn: "5d",
+      algorithm: "HS256",
+    })
+
+    return token
+  }
+
+  checkToken(token: string): string {
+    try {
+      const response = verify(token, this.jwtSecret)
+      if (!response) throw new CustomError(401, "Token must be a valid token")
+      const { data } = response as JwtPayload
+
+      return data
+    } catch (err) {
+      throw new CustomError(401, "Token must be a valid token")
+    }
+  }
+}
+
+export default tokenProvider
