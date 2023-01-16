4dc6333cc88ca6967e0d6ff8f3d6151aa2c7c95c
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-16 17:06:03 -0300

    feat(Pages): Criando pagina de login e adiconando a route

---
+++ app/client/src/pages/login/index.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+// import Form from "../components/form"
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <div>{/* <Form /> */}</div>
+    </div>
+  )
+}
+
+export default Login
--- app/client/src/routes/index.js
+++ app/client/src/routes/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
+import Login from "../pages/login"
 
 import NotFound from "../pages/NotFound"
 
 function RoutesApp() {
   return (
     <Routes>
-      <Route exact path="/" element={<div>login</div>} />
+      <Route exact path="/" element={<Login />} />
       <Route exact path="/users" element={<div>users</div>} />
       <Route exact path="/status" element={<div>status</div>} />
       <Route exact path="/dogs" element={<div>dogs</div>} />
