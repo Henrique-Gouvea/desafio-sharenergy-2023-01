e8edcf64a5c346cd8b0848d2b38492c2dead37e8
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-16 14:53:59 -0300

    feat(Middleware): Criando tokenMiddleware para verificação da existencia do token

---
+++ app/server/src/infrastructure/middleware/tokenMiddleware.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express"
+import CustomError from "../helpers/errors/CustomErrors"
+import { IToken } from "../interfaces/providers/IToken"
+
+class tokenMiddleware {
+  constructor(private token: IToken) {}
+
+  checkTokenMiddleware(req: Request, res: Response, next: NextFunction): void {
+    try {
+      const authHeader = req.headers.authorization
+
+      if (!authHeader) {
+        throw new CustomError(401, "Invalid token")
+      }
+
+      const data = this.token.checkToken(authHeader)
+      req.body.user = data
+
+      next()
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+export default tokenMiddleware
