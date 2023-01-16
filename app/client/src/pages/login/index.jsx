61c08929ff1b627c77a31a77e4a79095f2358c47
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-16 20:25:26 -0300

    feat(Pages): Adicionando estado aos componentes

--- app/client/src/pages/login/index.jsx
+++ app/client/src/pages/login/index.jsx
@@ -1,25 +1,57 @@
-import React from "react"
+import React, { Component } from "react"
 import Input from "../../components/input"
 import Button from "../../components/button"
 
-function Login() {
-  return (
-    <div>
-      <form>
-        <Input id="login-name" type="text" placeholder="Name"></Input>
-        <Input
-          id="login-password"
-          type="password"
-          placeholder="Password"
-        ></Input>
-        <Button type="submit">Login</Button>
-        <div>
-          <Input type="checkbox" name="remember" checked={false} />
-          <label id="remember-label">Remember Me</label>
-        </div>
-      </form>
-    </div>
-  )
+class Login extends Component {
+  state = {
+    username: "",
+    password: "",
+    isChecked: false,
+  }
+
+  handleChecked() {
+    this.setState({ isChecked: !this.state.isChecked })
+  }
+
+  render() {
+    const { username, password, isChecked } = this.state
+    return (
+      <div>
+        <form>
+          <Input
+            id="login-name"
+            type="text"
+            placeholder="Name"
+            value={username}
+            onChange={(event) =>
+              this.setState({ username: event.target.value })
+            }
+          ></Input>
+          <Input
+            id="login-password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) =>
+              this.setState({ password: event.target.value })
+            }
+          ></Input>
+          <div>
+            <Input
+              type="checkbox"
+              name="remember"
+              checked={isChecked}
+              onChange={() => this.handleChecked()}
+            />
+            <label id="remember-label" onChange={() => this.handleChecked()}>
+              Remember Me
+            </label>
+          </div>
+          <Button type="submit">Login</Button>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default Login
