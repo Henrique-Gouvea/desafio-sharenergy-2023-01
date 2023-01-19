9df7d097b2de89aebb995225e1b467b528ab493a
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-19 18:13:18 -0300

    feat(Components): Adicioando key nos compoenentes body e head table

--- app/client/src/components/bodyTable/index.jsx
+++ app/client/src/components/bodyTable/index.jsx
@@ -5,8 +5,8 @@ function BodyTable({ body }) {
   return (
     <tbody>
       {body ? (
-        body.map((bodyUnit) => (
-          <tr>
+        body.map((bodyUnit, index) => (
+          <tr key={index}>
             <td>{bodyUnit.username}</td>
             <td>{bodyUnit.email}</td>
             <td>
--- app/client/src/components/headTable/index.jsx
+++ app/client/src/components/headTable/index.jsx
@@ -4,8 +4,8 @@ function HeadTable({ headerTH }) {
   return (
     <thead clasName="table-header">
       <tr className="table-header-row">
-        {headerTH.map((header) => (
-          <th>{header}</th>
+        {headerTH.map((header, index) => (
+          <th key={index}>{header}</th>
         ))}
       </tr>
     </thead>
