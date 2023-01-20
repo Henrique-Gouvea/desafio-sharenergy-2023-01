e9ae808b52482c45cab0cdbf4430218a5d812d43
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-20 19:47:13 -0300

    style(Components): Fazendo css para pagina de usuarios

--- app/client/src/components/cardUser/index.jsx
+++ app/client/src/components/cardUser/index.jsx
@@ -2,12 +2,24 @@ import React from "react"
 
 function CardUser({ user }) {
   return (
-    <div>
-      <img src={user.picture.medium} alt="Picture user" />
-      <p>Nome: {user.name.first + user.name.last}</p>
-      <p>Idade: {user.dob.age}</p>
-      <p>Email: {user.email}</p>
-      <p>Username: {user.login.username}</p>
+    <div className="flex justify-center p-2">
+      <div className="rounded-lg shadow-lg bg-white w-96 h-96 p-1">
+        <img
+          src={user.picture.medium}
+          className="mx-auto bg-gradient-to-b rounded-full w-80 h-80 relative overflow-hidden mt-10 md:h-20 md:w-20"
+          alt="Picture user"
+        />
+        <div className="p-4">
+          <p className="text-gray-700 text-base mb-4">
+            Nome: {user.name.first + user.name.last}
+          </p>
+          <p class="text-gray-700 text-base mb-4">Idade: {user.dob.age}</p>
+          <p class="text-gray-700 text-base mb-4">Email: {user.email}</p>
+          <p class="text-gray-700 text-base mb-4">
+            Username: {user.login.username}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
--- app/client/src/pages/users/index.jsx
+++ app/client/src/pages/users/index.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react"
 import HttpService from "../../service/HttpService"
 import UrlService from "../../service/UrlService"
 import CardUser from "../../components/cardUser"
+import { RxDotFilled } from "react-icons/rx"
 
 class Users extends Component {
   constructor(props) {
     super(props)
     this.state = {
       users: "",
+      currentIndex: 0,
     }
     this.httpService = new HttpService()
     this.urlService = new UrlService()
@@ -16,20 +18,49 @@ class Users extends Component {
   async componentDidMount() {
     const { randomUser } = this.urlService
     const { data } = await this.httpService.get(randomUser(10))
+    const { currentIndex } = this.state
+    console.log(data.results[currentIndex])
     this.setState({ users: data.results })
   }
 
+  goToUser = (userIndex) => {
+    this.setState({ currentIndex: userIndex })
+  }
+
   render() {
-    const { users } = this.state
+    const { users, currentIndex } = this.state
     return (
       <div>
         {users ? (
-          users.map((user) => (
-            <CardUser key={user.login.username} user={user} />
-          ))
+          <CardUser
+            key={currentIndex}
+            user={users[currentIndex]}
+            onClick={() => this.goToUser(currentIndex)}
+          >
+            <RxDotFilled />
+          </CardUser>
         ) : (
           <></>
         )}
+        <div className="flex justify-center p-2">
+          {users ? (
+            users.map((_user, userIndex) => (
+              <div
+                key={userIndex}
+                onClick={() => this.goToUser(userIndex)}
+                className={
+                  currentIndex === userIndex
+                    ? "text-2xl bg-white cursor-pointer"
+                    : "text-2xl cursor-pointer"
+                }
+              >
+                <RxDotFilled />
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     )
   }
